a9b7e4317a33097df3ccb5a418177b47e1bece92
YasheshP458 <83372272+YasheshP458@users.noreply.github.com>
2021-04-28 17:03:05 -0400

    Add files via upload

---
+++ Lab 6/foo.java
@@ -0,0 +1,14 @@
+public class foo {
+	public static void main(String[] args) {
+		int x=0;
+		int i=1;
+		do {
+			int s= i*i;
+			x= s+x;
+			i= i+1;
+		}
+		while(i<= 3);
+		System.out.println("x= " + x);
+	}
+}
+
---
+++ Lab 6/hello.sh
@@ -0,0 +1,30 @@
+#!/bin.bash
+
+echo "Welcome to computer science society."
+
+#Yashesh Patel
+#ypatel16@student.gsu.edu
+
+echo "Date= `date`"
+
+echo */ | wc
+
+echo $PATH
+
+echo $USER
+
+echo $SHELL
+
+echo "Disk Usage= "
+
+echo "`df`"
+
+echo "Please, could you laon me "$"25.00?"
+
+echo "if x =2, x*x = 4, x/2 = 1"
+
+echo "`ls *.sh | grep c`"
+
+echo "Good Bye"
+
+echo "Current Hour = `date +"%H"`"
